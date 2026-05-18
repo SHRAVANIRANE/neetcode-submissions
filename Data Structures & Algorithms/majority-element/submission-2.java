fb39943617d1bbce6a50c4b5aac20d16d0a155c0
Shravani Rane <117579713+SHRAVANIRANE@users.noreply.github.com>
2026-05-18 19:33:43 +0530

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.java
@@ -0,0 +1,20 @@
+class Solution {
+        public int majorityElement(int[] nums) 
+        {
+                int count=0;
+                int candidate=nums[0];
+                for(int i=0;i<nums.length;i++){
+                    if(nums[i]!=candidate){
+                        count--;
+                    }else{
+                        count++;
+                    }
+                    if(count==0)
+                    {
+                        candidate=nums[i];
+                        count=1;
+                    }
+                }
+                return candidate;
+        }
+}
